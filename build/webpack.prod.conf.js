0b697dce9dca8fd616925a29fa16463c8607a39e
Michael Baudino <michael.baudino@alpine-lab.com>
2020-07-23 16:05:43 +0200

    Do not minify/uglify production build

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,10 +1,9 @@
 const webpack = require('webpack')
 const config = require('./webpack.base.conf')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const path = require('path')
 
-config.output.filename = 'vue-hotel-datepicker.min.js'
+config.output.filename = 'vue-hotel-datepicker.js'
 config.output.libraryTarget = 'umd'
 config.output.library = 'HotelDatePicker'
 
@@ -27,29 +26,6 @@ config.plugins = (config.plugins || []).concat([
 
   new ExtractTextPlugin({
     filename: 'vue-hotel-datepicker.min.css',
-  }),
-
-  new UglifyJSPlugin({
-    sourceMap: true,
-
-    compress: {
-      warnings: false,
-      drop_debugger: true,
-      drop_console: true,
-      screw_ie8: true,
-      global_defs: {
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-      },
-    },
-
-    mangle: {
-      screw_ie8: true,
-    },
-
-    output: {
-      comments: false,
-      screw_ie8: true,
-    }
   }),
 
   new webpack.optimize.OccurrenceOrderPlugin(),
